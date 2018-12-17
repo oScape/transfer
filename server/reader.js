3d8b0dc7433a8d7e512b129cebab5e8378d482f0
oScape <benoit.chassignol@gmail.com>
2018-12-18 00:09:42 +0100

    [PARSE] Parsing file

--- server/reader.js
+++ server/reader.js
@@ -1,10 +1,18 @@
 const fs = require( 'fs' );
 
-fs.readFile( './external/hello.txt', ( err, data ) => 
+fs.readFile( './external/hello.txt', 'utf8', ( err, data ) => 
 {
     if ( err )
     {
         throw err;
     }
-    console.log( data );
+
+    let lines = 
+        // split par ligne
+        data.split( '\n' )
+            // on enlève le retour chariot
+            .map( line => line.replace( '\r', '' ) )
+            // on split entre chaque element
+            .map( line => line.split( ': ' ) );    
+    console.log( lines );
 } );
