cf18d9dbd6ff222d8ce4e79908c1bc0ebd7ee200
oScape <benoit.chassignol@gmail.com>
2019-07-15 13:05:41 +0200

    [V1] add form styles

--- app/App.js
+++ app/App.js
@@ -10,18 +10,22 @@ export default function App() {
 
     return (
         <div className="app">
+            <h1 >Formulaire d'envoi de planning :</h1>
             <form id="form" encType="multipart/form-data">
                 <div id="nameContainer">
                     <label htmlFor="name">Nom :</label>
                     <input name="name" id="name" />
+                    <span>Pas de caractères spéciaux acceptés</span>
                 </div>
                 <div id="phoneContainer">
                     <label htmlFor="phone">Téléphone :</label>
                     <input name="phone" id="phone" />
+                    <span>Format de numéro standard : 0123456789</span>
                 </div>
                 <div id="messageContainer">
                     <label htmlFor="message">Message :</label>
                     <textarea name="message" id="message"></textarea>
+                    <span>Pas de restriction de taille du message</span>
                 </div>
                 <button type="submit" id="button" onClick={(e) => handleClick(e)}>Valider</button>
             </form>
